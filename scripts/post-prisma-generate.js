28208f17b8cfdebe8f3ff8335e7e0731419af43e
jakobhahn <jakob.hahn@me.com>
2026-01-19 00:52:29 +0100

    Add default.js creation for Prisma client

--- scripts/post-prisma-generate.js
+++ scripts/post-prisma-generate.js
@@ -2,10 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const defaultDtsPath = path.join(__dirname, '../node_modules/.prisma/client/default.d.ts');
+const defaultJsPath = path.join(__dirname, '../node_modules/.prisma/client/default.js');
 const clientTsPath = path.join(__dirname, '../node_modules/.prisma/client/client.ts');
+const clientJsPath = path.join(__dirname, '../node_modules/.prisma/client/client.js');
 
-// Create default.d.ts if it doesn't exist and client.ts exists
+// Create default.d.ts and default.js if they don't exist and client files exist
 if (fs.existsSync(clientTsPath) && !fs.existsSync(defaultDtsPath)) {
   fs.writeFileSync(defaultDtsPath, "export * from './client'\n");
   console.log('Created default.d.ts for Prisma client');
+}
+
+if (fs.existsSync(clientJsPath) && !fs.existsSync(defaultJsPath)) {
+  fs.writeFileSync(defaultJsPath, "module.exports = require('./client');\n");
+  console.log('Created default.js for Prisma client');
 }
