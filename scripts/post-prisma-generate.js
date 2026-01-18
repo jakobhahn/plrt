91c2cf68c1289fd15b72bc6c0170b9640b1af023
jakobhahn <jakob.hahn@me.com>
2026-01-19 00:54:47 +0100

    Fix default.js creation in post-prisma-generate script

--- scripts/post-prisma-generate.js
+++ scripts/post-prisma-generate.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const defaultDtsPath = path.join(__dirname, '../node_modules/.prisma/client/default.d.ts');
-const defaultJsPath = path.join(__dirname, '../node_modules/.prisma/client/default.js');
-const clientTsPath = path.join(__dirname, '../node_modules/.prisma/client/client.ts');
-const clientJsPath = path.join(__dirname, '../node_modules/.prisma/client/client.js');
+const prismaClientDir = path.join(__dirname, '../node_modules/.prisma/client');
+const defaultDtsPath = path.join(prismaClientDir, 'default.d.ts');
+const defaultJsPath = path.join(prismaClientDir, 'default.js');
+const clientTsPath = path.join(prismaClientDir, 'client.ts');
 
-// Create default.d.ts and default.js if they don't exist and client files exist
+// Create default.d.ts if it doesn't exist and client.ts exists
 if (fs.existsSync(clientTsPath) && !fs.existsSync(defaultDtsPath)) {
   fs.writeFileSync(defaultDtsPath, "export * from './client'\n");
   console.log('Created default.d.ts for Prisma client');
 }
 
-if (fs.existsSync(clientJsPath) && !fs.existsSync(defaultJsPath)) {
+// Create default.js - it's needed at runtime even if client.js doesn't exist yet
+// The client will be bundled by Next.js
+if (!fs.existsSync(defaultJsPath)) {
   fs.writeFileSync(defaultJsPath, "module.exports = require('./client');\n");
   console.log('Created default.js for Prisma client');
 }
