3009b8747868f4330e661fa8d31db5eca6bfb290
jakobhahn <jakob.hahn@me.com>
2026-01-19 01:07:33 +0100

    Update post-prisma-generate script for new output path and add generated to gitignore

--- scripts/post-prisma-generate.js
+++ scripts/post-prisma-generate.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const prismaClientDir = path.join(__dirname, '../node_modules/.prisma/client');
+const prismaClientDir = path.join(__dirname, '../generated/prisma-client');
 const defaultDtsPath = path.join(prismaClientDir, 'default.d.ts');
 const defaultJsPath = path.join(prismaClientDir, 'default.js');
 const clientTsPath = path.join(prismaClientDir, 'client.ts');
@@ -13,14 +13,10 @@ if (fs.existsSync(clientTsPath) && !fs.existsSync(defaultDtsPath)) {
 }
 
 // Create default.js - Prisma 7 generates TypeScript files only
-// @prisma/client/default.js requires '.prisma/client/default'
-// Since Next.js can't compile TypeScript in node_modules directly,
-// we need to create a wrapper that works with Next.js's module resolution
-// Solution: Create a .js file that uses ESM export syntax
-// Next.js should handle this during the build process
+// Since the output is now outside node_modules, Next.js can compile it
+// We use ESM export syntax which Next.js can process
 if (!fs.existsSync(defaultJsPath)) {
   // Use ESM export syntax - Next.js will compile client.ts during build
-  // The key is using export * which Next.js can process
   const defaultJsContent = `export * from './client';
 `;
   fs.writeFileSync(defaultJsPath, defaultJsContent);
