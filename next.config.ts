36a5016b8725cdc71bf81fff29ca88abcf0a41b4
jakobhahn <jakob.hahn@me.com>
2026-01-19 01:06:13 +0100

    Simplify next.config.ts - remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   // Allow transpiling Prisma client from node_modules
   // This allows Next.js to compile TypeScript files in @prisma/client
   transpilePackages: ['@prisma/client'],
-  // Configure webpack to handle .ts files in node_modules/.prisma/client
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Add .prisma/client to the list of directories that can be transpiled
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '.prisma/client': path.resolve(__dirname, 'node_modules/.prisma/client'),
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
